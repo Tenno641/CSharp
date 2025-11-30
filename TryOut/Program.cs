9a1371b2eee750a2ac3232ab008a71b510ace692
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-11-30 17:49:06 +0200

    Time struct

--- TryOut/Program.cs
+++ TryOut/Program.cs
@@ -10,6 +10,11 @@ Tuple<int, int> tuple = new(2, 4);
 Point tuplePoint = tuple;
 Console.WriteLine(tuplePoint);
 
+Time time1 = new Time(11, 15);
+Time time2 = new Time(15, 10);
+Time newTime = time1 + time2;
+Console.WriteLine(newTime);
+
 readonly struct Point : IEquatable<Point>
 {
     private readonly int _field;
@@ -41,4 +46,48 @@ readonly struct Point : IEquatable<Point>
     // public static explicit operator Point(Tuple<int, int> tuple) =>
       //  new Point(tuple.Item1, tuple.Item2);
 
+}
+
+public struct Time
+{
+    public int Hour { get; }
+    public int Minute { get; }
+
+    public Time(int hour, int minute)
+    {
+        if (hour < 0 || hour > 23)
+        {
+            throw new ArgumentOutOfRangeException(
+                "Hour is out of range of 0-23");
+        }
+        if (minute < 0 || minute > 59)
+        {
+            throw new ArgumentOutOfRangeException(
+                "Minute is out of range of 0-59");
+        }
+        Hour = hour;
+        Minute = minute;
+    }
+
+    public override string ToString() =>
+        $"{Hour.ToString("00")}:{Minute.ToString("00")}";
+
+    public static bool operator ==(Time time1, Time time2) =>
+        time1.Hour.Equals(time2.Hour) && time1.Minute.Equals(time2.Minute);
+
+    public static bool operator !=(Time time1, Time time2) =>
+        time1.Hour.Equals(time2.Hour) && time1.Minute.Equals(time2.Minute);
+
+    public static Time operator +(Time time1, Time time2)
+    {
+        int minutes = time1.Minute + time2.Minute;
+        int hours = time1.Hour + time2.Hour;
+
+        if (minutes > 59)
+        {
+            minutes -= 60;
+            ++hours;
+        }
+        return new Time(hours % 24, minutes);
+    }
 }
