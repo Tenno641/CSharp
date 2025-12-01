e47dc9c9cd6d85873ae4640701cc70c475248f20
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-12-01 21:07:27 +0200

    Querying API

--- TryOut/Program.cs
+++ TryOut/Program.cs
@@ -1,5 +1,24 @@
-﻿Person person1 = new Person("User-Defined", 22);
-Person person2 = new Person("User-Defined", 22);
-Console.WriteLine(person1.Equals(person2));
-Console.WriteLine(person1 == person2);
-public record Person(string Name, int Age); // positional record 
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+HttpClient httpClient = new();
+httpClient.BaseAddress = new Uri("http://localhost:5290/");
+HttpResponseMessage response = await httpClient.GetAsync("weatherforecast");
+string content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
+
+IEnumerable<Weather>? weathers = JsonSerializer.Deserialize<IEnumerable<Weather>>(content);
+if (weathers is null) return;
+foreach(Weather weather in weathers)
+{
+    Console.WriteLine(weather);
+}    
+readonly record struct Weather 
+{
+    [JsonPropertyName("date")]
+    public DateOnly Date { get; init; }
+    [JsonPropertyName("temperatureC")]
+    public int TemperatureC { get; init; }
+    [JsonPropertyName("summary")] public string Summary { get; init; }
+    [JsonPropertyName("temperatureF")] public int TemperatureF { get; init; }
+}
