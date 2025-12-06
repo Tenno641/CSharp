0223d15f8283aed3d68a65398b69c71c51261395
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-12-06 17:39:53 +0200

    Events

--- TryOut/Program.cs
+++ TryOut/Program.cs
@@ -1,23 +1,80 @@
-﻿using System.Globalization;
-using System.Text;
+﻿void Method1(int number, string word) => Console.WriteLine($"{number}.{word}");
+void Method2(int number, string word) => Console.WriteLine($"{number}v1.2 . {word}v1.2");
 
-decimal price = 252.252m;
+Method methodsChain = Method1;
+methodsChain += Method1;
+methodsChain += Method2;
 
-string result = string.Format("Price {0:C}", price);
-Console.WriteLine(result);
+methodsChain(1, "wordy");
 
-string date = string.Format("date {0:D}", DateTime.Now);
-Console.WriteLine(date);
+EmailPriceChangeNotifier emailPriceChangeNotifier = new(30_000);
+PushPriceChangeNotifier pushPriceChangeNotifier = new(25_000);
 
-CultureInfo currentCulture = CultureInfo.CurrentCulture;
-Console.WriteLine(currentCulture);
+GoldPriceReader goldPriceReader = new();
+goldPriceReader.priceRead += emailPriceChangeNotifier.Update;
+goldPriceReader.priceRead += pushPriceChangeNotifier.Update;
 
-CultureInfo.CurrentCulture = new CultureInfo("ar-SA");
-Console.OutputEncoding = Encoding.UTF8;
+for(int i = 0; i < 3; i++)
+{
+    goldPriceReader.ReadCurrentPrice();
+}
 
-string result2 = string.Format("Price {0:C}", price);
-Console.WriteLine(result2);
+public delegate void Method(int number, string word);
+public delegate void PriceRead(decimal price);
+public class GoldPriceReader
+{
+    public event PriceRead? priceRead;
 
-string date2 = string.Format("date {0:D}", DateTime.Now);
-Console.WriteLine(date2);
+    public void ReadCurrentPrice()
+    {
+        decimal currentGoldPrice = new Random().Next(
+            20_000, 50_000);
+        priceRead?.Invoke(currentGoldPrice);
 
+    }
+}
+
+public class EmailPriceChangeNotifier
+{
+    private readonly decimal _notificationThreshold;
+
+    public EmailPriceChangeNotifier(
+        decimal notificationThreshold)
+    {
+        _notificationThreshold = notificationThreshold;
+    }
+
+    public void Update(decimal price)
+    {
+        if (price > _notificationThreshold)
+        {
+            //imagine this is actually sending an email
+            Console.WriteLine(
+                $"Sending an email saying that " +
+                $"the gold price exceeded {_notificationThreshold} " +
+                $"and is now {price}\n");
+        }
+    }
+}
+
+public class PushPriceChangeNotifier{
+    private readonly decimal _notificationThreshold;
+
+    public PushPriceChangeNotifier(
+        decimal notificationThreshold)
+    {
+        _notificationThreshold = notificationThreshold;
+    }
+
+    public void Update(decimal price)
+    {
+        if (price > _notificationThreshold)
+        {
+            //imagine this is actually sending a push notification
+            Console.WriteLine(
+                $"Sending a push notification saying that " +
+                $"the gold price exceeded {_notificationThreshold} " +
+                $"and is now {price}\n");
+        }
+    }
+}
