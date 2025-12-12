c1d37e7e70fd50db6261ff60b39edfc3a613ea53
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2025-12-12 02:30:53 +0200

    Handling Exception in threads

--- ThreadingApp/Program.cs
+++ ThreadingApp/Program.cs
@@ -1,18 +1,44 @@
-﻿ static void RunThreads()
+﻿var cancellationTokenSource = new CancellationTokenSource();
+
+try
 {
-    Thread[] threads = new Thread[]
+    var task = Task.Run(() => NeverEndingMethod(cancellationTokenSource), cancellationTokenSource.Token);
+    var task2 = Task.Run(() => NeverEndingMethod2(cancellationTokenSource), cancellationTokenSource.Token);
+    Task.WaitAll(task, task2);
+} catch(AggregateException ex)
+{
+    foreach(Exception exception in ex.InnerExceptions)
     {
-                new Thread(Greet),
-                new Thread(Greet),
-                new Thread(Greet)
-    };
-    foreach (Thread thread in threads)
+        Console.WriteLine(exception.Message);
+        Console.WriteLine(exception);
+    }
+}
+//string userInput;
+//do
+//{
+//    userInput = Console.ReadLine();
+//} while (userInput != "cancel");
+//cancellationTokenSource.Cancel();
+
+void NeverEndingMethod(CancellationTokenSource tokenSource)
+{
+    while (true)
     {
-        thread.Start();
+        //tokenSource.Token.ThrowIfCancellationRequested();
+        throw new NullReferenceException("lol awy");
+        throw new NullReferenceException("el mrar el tafe7");
+        Thread.Sleep(1000);
+        Console.WriteLine("Another one");
     }
 }
 
- static void Greet()
+void NeverEndingMethod2(CancellationTokenSource tokenSource)
 {
-    Console.WriteLine($"Hello from thread with ID: {Thread.CurrentThread.ManagedThreadId}");
+    while (true)
+    {
+        //tokenSource.Token.ThrowIfCancellationRequested();
+        throw new NullReferenceException("el mrar el tafe7");
+        Thread.Sleep(1000);
+        Console.WriteLine("Another one");
+    }
 }
